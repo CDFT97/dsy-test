d217bb048af0d51885d86d1e8d9d9bca7687fa4a
Cesar Febres <febrescesar7@gmail.com>
2023-10-19 08:45:45 -0400

    limpieza

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,10 +37,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('departments', DepartmentController::class);
-    Route::resource('employees', EmployeeController::class);
-    Route::get('graphic',[EmployeeController::class,'EmployeeByDepartment'])->name('graphic');
-    Route::get('reports',[EmployeeController::class,'reports'])->name('reports');
 
 });
 
